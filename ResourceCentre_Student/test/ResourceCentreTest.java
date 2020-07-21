6e0acc3b902276a106e3a12c8511d659fac00acf
19029087 <19029087@DESKTOP-CM57HC0>
2020-07-21 15:32:34 +0800

    view

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -105,7 +105,6 @@ public class ResourceCentreTest {
 
 				testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n","CB0011", "My Google Chromebook 1st", "Yes", "", "Mac OS");
 				testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n","CB0012", "SAMSUNG Chromebook 4+", "Yes", "", "Win 10");
-			
 				assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
 
 	}
